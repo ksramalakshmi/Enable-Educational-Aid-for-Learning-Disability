30cb50118043b0b1ada517f7157b930fd457f1ce
K S Ramalakshmi <rlsri2305@gmail.com>
2023-03-10 21:57:07 +0530

    second commit

--- main.py
+++ main.py
@@ -12,16 +12,12 @@ def main():
     st.title("Enable Educational Aid")
     st.subheader("A tool to help you learn your lessons")
 
-    feature_menu = ["Choose what you want to do!", "Learn English Lessons!", "Quiz Yourself!", "Summarise Lesson!", "Read aloud with me!", "Meet your companion Jago!"]
+    feature_menu = ["Choose what you want to do!", "Learn English Lessons!", "Read aloud with me!", "Meet your companion Jago!"]
 
     feature_choice = st.sidebar.selectbox("It's study time!", feature_menu)
     match feature_choice:
         case "Learn English Lessons!":
             learn.learn_page()
-        case "Quiz Yourself!":
-            st.write("Quiz Yourself!")
-        case "Summarise Lesson!":
-            summary.summarise()
         case "Read aloud with me!":
             readaloud.read_aloud()
         case "Meet your companion Jago!":
